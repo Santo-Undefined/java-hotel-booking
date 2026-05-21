1ba3fe5be63091562f0ebcff08d9c67aab734ed2
Priyanshu A <priyanshua@Priyanshus-MacBook-Pro.local>
2026-05-21 11:54:17 +0530

    creates packages for controller, repository and service

---
+++ src/main/java/com/tw/hotel/controller/HotelController.java
@@ -0,0 +1,7 @@
+package com.tw.hotel.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HotelController {
+}
---
+++ src/main/java/com/tw/hotel/repository/HotelRepository.java
@@ -0,0 +1,4 @@
+package com.tw.hotel.repository;
+
+public class HotelRepository {
+}
