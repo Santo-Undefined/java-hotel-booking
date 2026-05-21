b41a9600a2aba993af06faac0fd62589067a8c44
Rashmika Gandra <rashmikagandra@Rashmikas-MacBook-Pro.local>
2026-05-21 16:09:46 +0530

    Adds docker compose to run db and server in different containers

--- src/test/java/com/tw/hotel/service/BookingServiceTest.java
+++ src/test/java/com/tw/hotel/service/BookingServiceTest.java
@@ -4,19 +4,22 @@ import com.tw.hotel.repository.BookingRepository;
 import com.tw.hotel.requestDto.BookingRequest;
 import com.tw.hotel.requestDto.BookingStatus;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.mongodb.test.autoconfigure.AutoConfigureDataMongo;
 import org.springframework.boot.test.context.SpringBootTest;
-
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 
 @SpringBootTest
+@AutoConfigureDataMongo
 class BookingServiceTest {
+    @Autowired
+    private BookingRepository bookingRepository;
 
     @Test
     void shouldBookAHotel() {
         final IdGenerator mockGenerator = mock(IdGenerator.class);
-        final BookingRepository mockRepo = mock(BookingRepository.class);
-        BookingService bookingService = new BookingService(mockRepo, mockGenerator);
+        BookingService bookingService = new BookingService(bookingRepository, mockGenerator);
         BookingRequest request = new BookingRequest(5, 3);
         BookingStatus bookingStatus = bookingService.bookHotel(request);
         assertEquals(bookingStatus.getHotel_id(), 5);
